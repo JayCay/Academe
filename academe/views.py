59d8eeb5cba8f7837be07d911b86a34f267eb8b4
Gab De Jesus <quillot2112@gmail.com>
2017-11-04 22:29:07 +0900

    Added login required decorators, html design, and showing of specific prof

---
+++ academe/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from profs.models import Prof
+from profs.forms import ProfForm
+
+# Create your views here.
+def index(request):
+	if(request.method=='POST'):
+		form = ProfForm(request.POST)
+		if(form.is_valid()):
+			form.save()
+			# Return to profs_index using reverse so only need to change url in settings.py
+			return redirect(reverse('profs_index'))
+
+	profs = Prof.objects.all()
+	return render(request, 'profs/index.html', {'prof_form':ProfForm, 'profs': profs})
